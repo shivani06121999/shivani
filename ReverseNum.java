b57e3dbd33428425255bfa59142b607a006792d8
unknown <shivanipaidimalla12@gmail.com>
2022-10-04 07:47:52 +0530

    [shivani]code has added

---
+++ ReverseNum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class ReverseNum{
+public static void main(String args[]){
+String num,rev="";
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the num");
+num=sc.nextLine();
+int l=num.length();
+for(int i=l-1;i>=0;i--)
+rev=rev+num.charAt(i);
+if(num.equals(rev))
+System.out.println("entered num is palindrome "+rev);
+else
+System.out.println("entered num is not palindrome "+rev);
+}}
